8acd1643f8038ad73e00f1728513374974026c3a
Papsy <57874831+uxp123@users.noreply.github.com>
2020-10-18 14:01:31 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+	<title>E-schools Social Network</title>
+    <link rel="icon" href="images/logo.png" type="image/png" sizes="16x16"> 
+    
+    <link rel="stylesheet" href="css/main.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/color.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+</head>
+<body>
+<!--<div class="se-pre-con"></div>-->
+<div class="theme-layout">
+	<div class="container-fluid pdng0">
+		<div class="row merged">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="land-featurearea">
+					<div class="land-meta">
+						<h1>E-schools Social Network</h1>
+						<p>
+							Experience the power of education.
+						</p>
+						<div class="">
+							<span><img src="images/logo.png" alt=""></span>
+						</div>
+						 <a href="index.php" title="" class="folow-me" style="text-decoration:underline; margin-top:50px">Go to home</a> 
+					</div>	
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="login-reg-bg">
+					<div class="log-reg-area sign">
+						<h2 class="log-title">Login</h2>
+							<p>
+								Don’t use E-schools Yet? <a href="register.php" title="">Join now</a>
+							</p>
+						<form method="post">
+							<div class="form-group">	
+							  <input type="text" id="input" required="required"/>
+							  <label class="control-label" for="input">Username</label><i class="mtrl-select"></i>
+							</div>
+							<div class="form-group">	
+							  <input type="password" required="required"/>
+							  <label class="control-label" for="input">Password</label><i class="mtrl-select"></i>
+							</div>
+							<!--<div class="checkbox">
+							  <label>
+								<input type="checkbox" checked="checked"/><i class="check-box"></i>Always Remember Me.
+							  </label>
+							</div> -->
+							<a href="#" title="" class="forgot-pwd">Forgot Password?</a>
+							<div class="submit-btns">
+								<button class="mtr-btn signin" type="submit"><span>Login</span></button>
+								<!-- <button class="mtr-btn signup" type="button"><span>Register</span></button> -->
+							</div>
+						</form>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	
+	<script src="js/main.min.js"></script>
+	<script src="js/script.js"></script>
+
+</body>	
+</html>
---
+++ register.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+	<title>E-schools Social Network</title>
+    <link rel="icon" href="images/logo.png" type="image/png" sizes="16x16"> 
+    
+    <link rel="stylesheet" href="css/main.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/color.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+</head>
+<body>
+<!--<div class="se-pre-con"></div>-->
+<div class="theme-layout">
+	<div class="container-fluid pdng0">
+		<div class="row merged">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="land-featurearea">
+					<div class="land-meta">
+						<h1>E-schools Social Network</h1>
+						<p>
+							Experience the power of education.
+						</p>
+						<div class="">
+							<span><img src="images/logo.png" alt=""></span>
+						</div>
+						<a href="index.php" title="" class="folow-me" style="text-decoration:underline; margin-top:50px">Go to home</a> 
+					</div>	
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="login-reg-bg">
+
+					<div class="log-reg-area">
+						<h2 class="log-title">Register</h2>
+							<p>
+								
+							</p>
+						<form method="post">
+							<div class="form-group">	
+							  <input type="text" required="required"/>
+							  <label class="control-label" for="input">First & Last Name</label><i class="mtrl-select"></i>
+							</div>
+							<div class="form-group">	
+							  <input type="email" required="required"/>
+							  <label class="control-label" for="input">Email</label><i class="mtrl-select"></i>
+							</div>
+                            <div class="form-group">	
+							  <input type="text" />
+							  <label class="control-label" for="input">Mobile Number</label><i class="mtrl-select"></i>
+							</div>
+							<div class="form-group">	
+							  <input type="password" required="required"/>
+							  <label class="control-label" for="input">Password</label><i class="mtrl-select"></i>
+							</div>
+                            <div class="form-group">	
+							  <input type="password" required="required"/>
+							  <label class="control-label" for="input">Confirm Password</label><i class="mtrl-select"></i>
+							</div>
+							<div class="form-radio">
+							  <div class="radio">
+								<label>
+								  <input type="radio" name="radio" /><i class="check-box"></i>Male
+								</label>
+							  </div>
+							  <div class="radio">
+								<label>
+								  <input type="radio" name="radio"/><i class="check-box"></i>Female
+								</label>
+							  </div>
+							</div>
+							
+							<!-- <div class="checkbox">
+							  <label>
+								<input type="checkbox" checked="checked"/><i class="check-box"></i>Accept Terms & Conditions ?
+							  </label>
+							</div> -->
+							<a href="login.php" title="" class="" style="color:#088dcd; text-decoration:underline">Already have an account</a>
+							<div class="submit-btns">
+								<button class="mtr-btn signup" type="submit"><span>Register</span></button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	
+	<script src="js/main.min.js"></script>
+	<script src="js/script.js"></script>
+
+</body>	
+</html>
