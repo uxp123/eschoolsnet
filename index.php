b40d55fe6ceb700c7f7a7ce326bf0d30f761e157
Papsy <57874831+uxp123@users.noreply.github.com>
2020-10-18 13:59:49 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,484 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+	<title>E-schools Social Network</title>
+    <link rel="icon" href="images/logo.png" type="image/png" sizes="16x16"> 
+    
+    <link rel="stylesheet" href="css/main.min.css">
+    <link rel="stylesheet" href="css/strip.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/color.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+</head>
+<body>
+<!--<div class="se-pre-con"></div>-->
+<div class="theme-layout">
+	
+	<div class="responsive-header">
+		<div class="mh-head first Sticky">
+			<span class="mh-btns-left">
+				<a class="" href="#menu"><i class="fa fa-align-justify"></i></a>
+			</span>
+			<span class="mh-text">
+				<a href="newsfeed.html" title=""><img src="images/logo.png" alt=""></a>
+			</span>
+			<span class="mh-btns-right">
+				<a class="fa fa-sliders" href="#shoppingbag"></a>
+			</span>
+		</div>
+		<div class="mh-head second">
+			<form class="mh-form">
+				<input placeholder="search" />
+				<a href="#/" class="fa fa-search"></a>
+			</form>
+		</div>
+		<nav id="menu" class="res-menu">
+			<ul>
+				<li><span>Home</span>
+					<ul>
+						<li><a href="index.html" title="">Home Social</a></li>
+						<li><a href="index2.html" title="">Home Social 2</a></li>
+						<li><a href="index-company.html" title="">Home Company</a></li>
+						<li><a href="landing.html" title="">Login page</a></li>
+						<li><a href="logout.html" title="">Logout Page</a></li>
+						<li><a href="newsfeed.html" title="">news feed</a></li>
+					</ul>
+				</li>
+				<li><span>Time Line</span>
+					<ul>
+						<li><a href="time-line.html" title="">timeline</a></li>
+						<li><a href="timeline-friends.html" title="">timeline friends</a></li>
+						<li><a href="timeline-groups.html" title="">timeline groups</a></li>
+						<li><a href="timeline-pages.html" title="">timeline pages</a></li>
+						<li><a href="timeline-photos.html" title="">timeline photos</a></li>
+						<li><a href="timeline-videos.html" title="">timeline videos</a></li>
+						<li><a href="fav-page.html" title="">favourit page</a></li>
+						<li><a href="groups.html" title="">groups page</a></li>
+						<li><a href="page-likers.html" title="">Likes page</a></li>
+						<li><a href="people-nearby.html" title="">people nearby</a></li>
+						
+						
+					</ul>
+				</li>
+				<li><span>Account Setting</span>
+					<ul>
+						<li><a href="create-fav-page.html" title="">create fav page</a></li>
+						<li><a href="edit-account-setting.html" title="">edit account setting</a></li>
+						<li><a href="edit-interest.html" title="">edit-interest</a></li>
+						<li><a href="edit-password.html" title="">edit-password</a></li>
+						<li><a href="edit-profile-basic.html" title="">edit profile basics</a></li>
+						<li><a href="edit-work-eductation.html" title="">edit work educations</a></li>
+						<li><a href="messages.html" title="">message box</a></li>
+						<li><a href="inbox.html" title="">Inbox</a></li>
+						<li><a href="notifications.html" title="">notifications page</a></li>
+					</ul>
+				</li>
+				<li><span>forum</span>
+					<ul>
+						<li><a href="forum.html" title="">Forum Page</a></li>
+						<li><a href="forums-category.html" title="">Fourm Category</a></li>
+						<li><a href="forum-open-topic.html" title="">Forum Open Topic</a></li>
+						<li><a href="forum-create-topic.html" title="">Forum Create Topic</a></li>
+					</ul>
+				</li>
+				<li><span>Our Shop</span>
+					<ul>
+						<li><a href="shop.html" title="">Shop Products</a></li>
+						<li><a href="shop-masonry.html" title="">Shop Masonry Products</a></li>
+						<li><a href="shop-single.html" title="">Shop Detail Page</a></li>
+						<li><a href="shop-cart.html" title="">Shop Product Cart</a></li>
+						<li><a href="shop-checkout.html" title="">Product Checkout</a></li>
+					</ul>
+				</li>
+				<li><span>Our Blog</span>
+					<ul>
+						<li><a href="blog-grid-wo-sidebar.html" title="">Our Blog</a></li>
+						<li><a href="blog-grid-right-sidebar.html" title="">Blog with R-Sidebar</a></li>
+						<li><a href="blog-grid-left-sidebar.html" title="">Blog with L-Sidebar</a></li>
+						<li><a href="blog-masonry.html" title="">Blog Masonry Style</a></li>
+						<li><a href="blog-list-wo-sidebar.html" title="">Blog List Style</a></li>
+						<li><a href="blog-list-right-sidebar.html" title="">Blog List with R-Sidebar</a></li>
+						<li><a href="blog-list-left-sidebar.html" title="">Blog List with L-Sidebar</a></li>
+						<li><a href="blog-detail.html" title="">Blog Post Detail</a></li>
+					</ul>
+				</li>
+				<li><span>Portfolio</span>
+					<ul>
+						<li><a href="portfolio-2colm.html" title="">Portfolio 2col</a></li>
+						<li><a href="portfolio-3colm.html" title="">Portfolio 3col</a></li>
+						<li><a href="portfolio-4colm.html" title="">Portfolio 4col</a></li>
+					</ul>
+				</li>
+				<li><span>Support & Help</span>
+					<ul>
+						<li><a href="support-and-help.html" title="">Support & Help</a></li>
+						<li><a href="support-and-help-detail.html" title="">Support & Help Detail</a></li>
+						<li><a href="support-and-help-search-result.html" title="">Support & Help Search Result</a></li>
+					</ul>
+				</li>
+				<li><span>More pages</span>
+					<ul>
+						<li><a href="careers.html" title="">Careers</a></li>
+						<li><a href="career-detail.html" title="">Career Detail</a></li>
+						<li><a href="404.html" title="">404 error page</a></li>
+						<li><a href="404-2.html" title="">404 Style2</a></li>
+						<li><a href="faq.html" title="">faq's page</a></li>
+						<li><a href="insights.html" title="">insights</a></li>
+						<li><a href="knowledge-base.html" title="">knowledge base</a></li>
+					</ul>
+				</li>
+				<li><a href="about.html" title="">about</a></li>
+				<li><a href="about-company.html" title="">About Us2</a></li>
+				<li><a href="contact.html" title="">contact</a></li>
+				<li><a href="contact-branches.html" title="">Contact Us2</a></li>
+				<li><a href="widgets.html" title="">Widgts</a></li>
+			</ul>
+		</nav>
+		<nav id="shoppingbag">
+			<div>
+				<div class="">
+					<form method="post">
+						<div class="setting-row">
+							<span>use night mode</span>
+							<input type="checkbox" id="nightmode"/> 
+							<label for="nightmode" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Notifications</span>
+							<input type="checkbox" id="switch2"/> 
+							<label for="switch2" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Notification sound</span>
+							<input type="checkbox" id="switch3"/> 
+							<label for="switch3" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>My profile</span>
+							<input type="checkbox" id="switch4"/> 
+							<label for="switch4" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Show profile</span>
+							<input type="checkbox" id="switch5"/> 
+							<label for="switch5" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+					</form>
+					<h4 class="panel-title">Account Setting</h4>
+					<form method="post">
+						<div class="setting-row">
+							<span>Sub users</span>
+							<input type="checkbox" id="switch6" /> 
+							<label for="switch6" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>personal account</span>
+							<input type="checkbox" id="switch7" /> 
+							<label for="switch7" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Business account</span>
+							<input type="checkbox" id="switch8" /> 
+							<label for="switch8" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Show me online</span>
+							<input type="checkbox" id="switch9" /> 
+							<label for="switch9" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Delete history</span>
+							<input type="checkbox" id="switch10" /> 
+							<label for="switch10" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+						<div class="setting-row">
+							<span>Expose author name</span>
+							<input type="checkbox" id="switch11" /> 
+							<label for="switch11" data-on-label="ON" data-off-label="OFF"></label>
+						</div>
+					</form>
+				</div>
+			</div>
+		</nav>
+	</div><!-- responsive header -->
+	
+	<div class="topbar transparent">
+		<div class="logo">
+			<a title="" href="index.php"><img src="images/logo.png" alt="" width="60px" height="60px"></a>
+		</div>
+		<div class="menu-container" id="toggle">
+		  <a href="#" class="canvas-menu" >
+			<i class="fa fa-times fa-bars" aria-hidden="true"></i></a>
+		</div>
+		<div class="overlay" id="overlay">
+		  <nav class="overlay-menu">
+			<ul class="offcanvas-menu">
+				<li class="menu-item-has-children">
+					<a href="#" title="">Home</a>
+					<ul>
+						<li><a href="index.php" title="">Home Social</a></li>
+						<li><a href="index2.html" title="">Home Social 2</a></li>
+						
+					</ul>
+				</li>
+				<li class="menu-item-has-children">
+					<a href="#" title="">Our Blog</a>
+					<ul>
+						<li><a href="blog-grid-wo-sidebar.html" title="">Our Blog</a></li>
+						<li><a href="blog-grid-right-sidebar.html" title="">Blog with R-Sidebar</a></li>
+						<li><a href="blog-grid-left-sidebar.html" title="">Blog with L-Sidebar</a></li>
+						<li><a href="blog-masonry.html" title="">Blog Masonry Style</a></li>
+						<li><a href="blog-list-wo-sidebar.html" title="">Blog List Style</a></li>
+						<li><a href="blog-list-right-sidebar.html" title="">Blog List with R-Sidebar</a></li>
+						<li><a href="blog-list-left-sidebar.html" title="">Blog List with L-Sidebar</a></li>
+						<li><a href="blog-detail.html" title="">Blog Post Detail</a></li>
+					</ul>
+				</li>
+				<li class="menu-item-has-children">
+					<a href="#" title="">Shop Pages</a>
+					<ul>
+						<li><a href="shop.html" title="">Shop Products</a></li>
+						<li><a href="shop-masonry.html" title="">Shop Masonry Products</a></li>
+						<li><a href="shop-single.html" title="">Shop Detail Page</a></li>
+						<li><a href="shop-cart.html" title="">Shop Product Cart</a></li>
+						<li><a href="shop-checkout.html" title="">Product Checkout</a></li>
+					</ul>
+				</li>
+				<li class="menu-item-has-children">
+					<a href="#" title="">Our Portfolio</a>
+					<ul>
+						<li><a href="portfolio-2colm.html" title="">Portfolio 2col</a></li>
+						<li><a href="portfolio-3colm.html" title="">Portfolio 3col</a></li>
+						<li><a href="portfolio-4colm.html" title="">Portfolio 4col</a></li>
+					</ul>
+				</li>
+				<li class="menu-item-has-children">
+					<a href="#" title="">Support & Help</a>
+					<ul>
+						<li><a href="support-and-help.html" title="">Support & Help</a></li>
+						<li><a href="support-and-help-detail.html" title="">Support & Help Detail</a></li>
+						<li><a href="support-and-help-search-result.html" title="">Support & Help Search Result</a></li>
+					</ul>
+				</li>
+				<li class="menu-item-has-children">
+					<a href="#" title="">Company Forum</a>
+					<ul>
+						<li><a href="forum.html" title="">Forum Page</a></li>
+						<li><a href="forums-category.html" title="">Fourm Category</a></li>
+						<li><a href="forum-open-topic.html" title="">Forum Open Topic</a></li>
+						<li><a href="forum-create-topic.html" title="">Forum Create Topic</a></li>
+					</ul>
+				</li>
+				<li class="menu-item-has-children">
+					<a href="#" title="">Featured Pages</a>
+					<ul>
+						<li><a href="careers.html" title="">Careers</a></li>
+						<li><a href="career-detail.html" title="">Career Detail</a></li>
+						<li><a href="logout.html" title="">Logout Page</a></li>
+						<li><a href="404-2.html" title="">404 Errro Page</a></li>
+						<li><a href="aboutus.php" title="">About Us</a></li>
+						<li><a href="contactus.php" title="">Contact Us</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		  </nav>
+		</div>
+	</div><!-- topbar transparent header -->
+	
+	<div class="gap100 no-gap">
+        <div class="container-fluid">
+				<div class="row">
+					<div class="background-video">
+						<video class="bv-video" muted></video>
+						<div class="videobg-meta">
+							<h3 style="margin-left:50px"><i class="fa fa-smile-o"></i>Welcome to E-schools Social Network </h3>
+                            <div class="info">
+                            	<span>Our own social network</span>
+                                <p>
+                                Our multi-millionaire project brings the educational inclusion through the prominent aid of today's technology. Thus, the birth of E-Schools Net! We bring to you a range of features and functionalities including A.P.Is for the help of the student community with disabilities- from visual impairments, to the deaf-. Join us in our journey to make education much more inclusive... 
+                                </p>
+                                <a href="register.php" title="" class="btn">Sign up</a> 
+								<a href="login.php" title="" class="btn" style="margin-left:20px">Login</a> 
+                            </div>
+						</div>
+					</div>
+				</div>
+			</div>
+    </div><!-- background video featured area -->
+	
+	<section>
+		<div class="gap100 no-top overlap-75">
+			<div class="container">
+				<div class="servic-category">
+					<div class="row merged">
+						<div class="col-lg-4">
+							<div class="category-box">
+								<i class="fa fa-wordpress"></i>
+								<h2>E-Forums</h2>
+								<p>Want to have a collective brainstorming period? Here's the place to have it! Think, teach and learn  with others from a wide variety of people from all walks of life.</p>
+								<!-- <a href="#" class="main-btn" title="">Read More</a> -->
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="category-box selected">
+								<i class="fa fa-wordpress"></i>
+								<h2>Knowledge Base </h2>
+								<p>Want to share? Here you can have suggestions of sites which could help you with your studies.</p>
+								<!-- <a href="#" class="main-btn" title="">Read More</a> -->
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="category-box">
+								<i class="fa fa-wordpress"></i>
+								<h2>Chats and Events</h2>
+								<p> Meet up, talk and discuss with others. Have a good time learning something new about a new concept ;whatsoever. Have a great time and CONNECT!</p>
+								<!-- <a href="#" class="main-btn" title="">Read More</a> -->
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</section><!-- services section -->
+		
+	<section>
+		<div class="gap100 no-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="section-heading">
+							<h2>Our Team</h2>
+						</div>
+					</div>	
+					<div class="col-lg-3 col-sm-6">
+						<div class="our-teambox">
+							<figure><img src="images/papsy5.png" alt="" height="315px" width="263px"></figure>
+							<div class="team-info">
+								<h4>Philip Duodu Fynn</h4>
+								<span>Developer</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<div class="our-teambox">
+							<figure><img src="images/joseph3.png" alt=""  height="315px" width="263px"></figure>
+							<div class="team-info">
+								<h4>Joseph Ibiduma Apiriala-Atedoghu</h4>
+								<span>Developer</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<div class="our-teambox">
+							<figure><img src="images/kob2.png" alt="" height="315px" width="263px"></figure>
+							<div class="team-info">
+								<h4>Kwabena Acheampong Baidoo</h4>
+								<span>Writer and Designer</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<div class="our-teambox">
+							<figure><img src="images/mark3.png" alt="" height="315px" width="263px"></figure>
+							<div class="team-info">
+								<h4>Mark-Anthony Made</h4>
+								<span>Writer and Designer</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- our team -->
+	
+	
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4">
+					<div class="widget">
+						<div class="foot-logo">
+							<div class="logo">
+								<a href="index.php" title=""><img src="images/logo.png" alt="" height="60px" width="60px"></a>
+							</div>	
+							
+						</div>
+						<ul class="location">
+							<li>
+								<i class="ti-map-alt"></i>
+								<p>Aves International Academy, VRA Road Community 25, Tema</p>
+							</li>
+							<li>
+								<i class="ti-mobile"></i>
+								<p>(+233) 266-153097</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-4">
+					<div class="widget">
+						<div class="widget-title"><h4>follow</h4></div>
+						<ul class="list-style">
+							<li><i class="fa fa-twitter-square"></i><a href="https://www.facebook.com/avesacademy/" title="">Facebook</a></li>
+							<li><i class="fa fa-instagram"></i><a href="https://www.linkedin.com/company/avesinternationalacademy" title="">LinkedIn</a></li>
+							<li><i class="fa fa-google-plus-square"></i> <a href="https://www.instagram.com/avesacademy/?hl=en" title="">Instagram</a></li>
+							
+						</ul>
+					</div>
+				</div>
+				
+				
+				<div class="col-lg-2 col-md-4">
+					<div class="widget">
+						<div class="widget-title"><h4>Source Codes</h4></div> 
+						<ul class="colla-apps">
+							<li><a href="https://github.com/uxp123/eschoolsnet" title=""><i class=""></i>E-schoolsnet</a></li>
+							<li><a href="https://github.com/uxp123/eschoolsnet-middleware" title=""><i class=""></i>E-scholsnet middleware</a></li>
+							<li><a href="https://github.com/uxp123/eschoolsnet-microservices" title=""><i class=""></i>E-schoolsnet microservices</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+			</div>
+		</div>
+	</footer><!-- footer -->
+	<div class="bottombar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<span class="copyright">© E-school Social Network 2020. All rights reserved.</span>
+					<!-- <i><img src="images/credit-cards.png" alt=""></i> -->
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>	
+	
+	<script src="js/main.min.js"></script>
+	<script src="js/backgroundVideo.js"></script>
+	<script src="js/strip.pkgd.min.js"></script>
+	<script src="js/script.js"></script>
+	
+	<script>
+		jQuery(window).on('load',function() {
+			"use strict";
+			// video parallax for top featured
+			const backgroundVideo = new BackgroundVideo('.bv-video', {
+			  src: [
+				'videos/bg.MP4',
+			  ]
+			});
+		});
+			
+	</script>
+
+</body>	
+</html>
